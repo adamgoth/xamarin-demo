c0354580091dd97f689a9e998adfd01a9fcc28fc
Adam Goth <adamgoth@gmail.com>
2017-01-22 18:24:01 -0700

    Initial commit

---
+++ Quotes/QuotesPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Xamarin.Forms;
+
+namespace Quotes
+{
+	public partial class QuotesPage : ContentPage
+	{
+
+		private int _index = 0;
+		private string[] _quotes = new string[]
+		{
+			"Life is like riding a bicycle. To keep your balance, you must keep moving.",
+			"You can't blame gravity for falling in love.",
+			"Look deep into nature, and then you will understand everything better."
+		};
+
+		public QuotesPage()
+		{
+			InitializeComponent();
+		}
+
+		void Handle_Clicked(object sender, System.EventArgs e)
+		{
+			_index++;
+			if (_index >= _quotes.Length)
+				_index = 0;
+
+			currentQuote.Text = _quotes[_index];
+		}
+	}
+}
